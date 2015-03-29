b3cba9eca72ffde68c14ff3d12cc1e55f9d996d6
Alan Cao <alannesta@sina.com>
2015-03-29 17:38:23 -0400

    tabs

---
+++ app/scripts/PlaygroundController.js
@@ -0,0 +1,24 @@
+(function(){
+
+  angular
+       .module('users')
+       .controller('PlaygroundController', playgroundController);
+
+  function playgroundController(){
+    var self = this;
+    self.tabs = [{
+      title: 'tab1',
+      content: '<div>Tab1 content</div>'
+    },
+    {
+      title: 'tab2',
+      content: '<div>Tab2 content</div>'
+    }];
+
+    self.selectedIndex = 1;
+    self.onTabSelected = function(tab){
+      console.log('tab selected', tab);
+    }
+
+  }
+})();
