3a993e61609638281a62e444526f1785809da87e
Alan Cao <alannesta@sina.com>
2015-04-01 23:08:36 -0400

    md-gridlist

--- app/scripts/PlaygroundController.js
+++ app/scripts/PlaygroundController.js
@@ -9,6 +9,14 @@
     var self = this;
     var firebase = new Firebase(FIREBASE.BASE_URL);
     self.cards = $firebaseArray(firebase);
+
+    self.cards.$loaded().then(function(){
+      console.log('cards loaded');
+    }, function(err){
+      console.log(err);
+    });
+
+
     self.tabs = [{
       title: 'tab1',
       content: '<div>Tab1 content</div>'
