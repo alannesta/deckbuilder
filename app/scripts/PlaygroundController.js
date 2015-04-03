7b28291047faed44cf4afb3f7f7ff8c5f8be02f3
Alan Cao <alannesta@sina.com>
2015-04-02 23:32:34 -0400

    dupe data for pagination

--- app/scripts/PlaygroundController.js
+++ app/scripts/PlaygroundController.js
@@ -4,11 +4,18 @@
        .module('users')
        .controller('PlaygroundController', playgroundController);
 
-  function playgroundController($firebaseArray, FIREBASE){
+  function playgroundController($firebaseArray, FIREBASE, $scope){
 
     var self = this;
+
+    var currentPage = 0;
+    var perPage = 8;
+
+    self.currentCards = {};
+
     var firebase = new Firebase(FIREBASE.BASE_URL);
     self.cards = $firebaseArray(firebase);
+
 
     self.cards.$loaded().then(function(){
       console.log('cards loaded');
@@ -26,10 +33,36 @@
       content: '<div>Tab2 content</div>'
     }];
 
-    self.selectedIndex = 1;
-    self.onTabSelected = function(tab){
-      console.log('tab selected', tab);
-    }
+    self.nextPage = function(){
+      currentPage++;
+      //self.cards.forEach(function(card){
+      //  console.log(card);
+      //  delete card['$id'];
+      //  delete card['$priority'];
+      //  delete card['$$hashKey'];
+      //
+      //  firebase.push(card, function(err){
+      //    if(err){
+      //      console.log(err);
+      //    }else{
+      //      console.log('dupe success');
+      //    }
+      //  });
+      //});
+    };
+
+    $scope.$watch(function(){
+      return currentPage;
+    }, function(newVal){
+      var end = (self.cards.length > newVal*perPage + perPage)? newVal*perPage + perPage: self.cards.length;
+      self.currentCards = self.cards.slice(newVal*perPage, end);
+      console.log(self.currentCards);
+    });
+
+    //self.selectedIndex = 1;
+    //self.onTabSelected = function(tab){
+    //  console.log('tab selected', tab);
+    //}
 
   }
 })();
