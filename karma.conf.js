e52cc168356f55cd8a495c8b556c29300d9c7271
Alan Cao <alannesta@sina.com>
2015-04-08 11:51:24 -0400

    get the test up and running

--- karma.conf.js
+++ karma.conf.js
@@ -6,11 +6,13 @@ module.exports = function(config) {
             'app/bower_components/angular-route/angular-route.js',
             'app/bower_components/angular-aria/angular-aria.js',
             'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/bower_components/angular-animate/angular-animate.js',
             'app/bower_components/angular-material/angular-material.js',
             'app/bower_components/firebase/firebase.js',
             'app/bower_components/angularfire/dist/angularfire.js',
             'app/bower_components/angular-route/angular-route.js',
             'app/bower_components/underscore/underscore.js',
+            'app/scripts/app.js',       //!important have to explicitly load the module dependencies before all other files
             'app/scripts/**/*.js',
             'app/view*/**/*.js'
         ],
