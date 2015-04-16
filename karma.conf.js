bae54de3f630bce64af8a80259d80f08947bed64
Alan Cao <alannesta@sina.com>
2015-04-16 12:08:02 -0400

    karma-coverage configuration

--- karma.conf.js
+++ karma.conf.js
@@ -27,12 +27,30 @@ module.exports = function(config) {
             'karma-firefox-launcher',
             'karma-phantomjs-launcher',
             'karma-jasmine',
-            'karma-junit-reporter'
+            'karma-junit-reporter',
+            'karma-coverage'
         ],
 
         junitReporter: {
             outputFile: 'test_out/unit.xml',
             suite: 'unit'
+        },
+
+        //karma-coverage configuration
+
+        // coverage reporter generates the coverage
+        reporters: ['progress', 'coverage'],
+
+        preprocessors: {
+            // source files, that you wanna generate coverage for
+            // do not include tests or libraries
+            // (these files will be instrumented by Istanbul)
+            'app/scripts/**/*.js': ['coverage']
+        },
+        // optionally, configure the reporter
+        coverageReporter: {
+            type : 'html',
+            dir : 'coverage/'
         }
 
     });
