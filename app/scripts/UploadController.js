2944858d2cf0a848abe1278b819470c6cefb3870
Alan Cao <alannesta@sina.com>
2015-05-22 14:26:50 -0400

    basic workflow

--- app/scripts/UploadController.js
+++ app/scripts/UploadController.js
@@ -4,44 +4,48 @@
        .module('deckbuilder')
        .controller('UploadController', uploadController);
 
-  function uploadController($scope, FIREBASE){
+  function uploadController($scope){
 
-      var firebase = new Firebase(FIREBASE.BASE_URL);
       var self = this;
-      self.selections = [1, 2, 3, 4];
-      self.card = {};
-      //document.getElementById('image-upload').addEventListener('change', handleFileSelect);
 
-      function handleFileSelect(evt) {
-          var file = evt.target.files[0];
-          var reader = new FileReader();
-          reader.onload = (function() {
-              return function(e) {
-                  //var filePayload = e.target.result;
-                  $scope.$apply(function(){
-                      self.card.imageData = e.target.result;
-                      document.getElementById('preview').src = e.target.result;
-                  });
-
-              };
-          })();
-          reader.readAsDataURL(file);
-      }
+      self.step = 1;
+      self.selections = [];
 
       self.save = function(){
-          firebase.push(self.card, function(err){
-              if(err){
-                  console.log(err);
-              }else{
-                  console.log('success');
-              }
-          });
+
       };
 
       $scope.$watch(function(){
-          return self.card;
+          return self.rarity;
       }, function(val){
-          console.log(val)
+          if (val) {
+              self.selections.push(val);
+              self.step++;
+          }
+      });
+
+      $scope.$watch(function(){
+          return self.class;
+      }, function(val){
+          if (val) {
+              self.selections.push(val);
+              self.step++;
+          }
+      });
+
+      $scope.$watch(function(){
+          return self.cost;
+      }, function(val){
+          if (val) {
+              self.selections.push(val);
+              self.step++;
+          }
+      });
+
+      $scope.$watch(function(){
+          return self.selections;
+      }, function(){
+          self.step = self.selections.length + 1;
       }, true);
 
   }
