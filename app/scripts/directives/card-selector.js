791af5ffe780ec09e90d88c539fb3bb778af145b
Alan Cao <alannesta@sina.com>
2015-05-23 22:16:45 -0400

    construct senario in directive

--- app/scripts/directives/card-selector.js
+++ app/scripts/directives/card-selector.js
@@ -8,7 +8,10 @@ angular.module('deckbuilder').directive('cardSelector', function() {
         templateUrl: 'views/templates/card-selector.html',
         link: function(scope, element, attrs) {
             scope.finish = function() {
-                console.log(scope.onFinish);
+                //console.log(scope.onFinish);
+                var name = $('#set1 md-select')[0].val;
+                var count = $('#set1 md-select')[1].val;
+                scope.senario.push({name: name, count: count});
                 scope.onFinish();
             };
         }
